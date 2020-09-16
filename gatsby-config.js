39035b3f3bec19a7837ec6bac478660c9bbbae69
Derrick C <derrick.chow234@gmail.com>
2020-09-15 20:39:47 -0400

    gh pages configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+
+  module.exports = {
+    pathPrefix: "/reponame",
+  }
+
